10c59ca71452d8c191b94dda7fc44da4a6cf2fb1
EeyoungSong <eeyoungsong@gmail.com>
2025-11-22 20:56:44 +0900

    feat: API_BASE_URL 수정 3

--- services/api-client.ts
+++ services/api-client.ts
@@ -2,7 +2,7 @@ import { ApiOptions } from "@/types/common";
 
 // TODO: 환경변수 설정 필요
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://4.230.112.41/api/v1";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://4.230.112.41/wu-be/api/v1";
 
 export class APIError extends Error {
   status?: number;
